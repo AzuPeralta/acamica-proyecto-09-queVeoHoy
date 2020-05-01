6afb3fdbe4fd18a25a427df902d5bea756ef63fa
Azu Peralta <marielajperalta@gmail.com>
2020-05-01 01:30:55 -0300

    se desarrollan los fitros y la pagina por default

--- que-veo-hoy-recursos/servidor/controladores/controlador.js
+++ que-veo-hoy-recursos/servidor/controladores/controlador.js
@@ -9,22 +9,111 @@ function traerTodasLasPeliculas(req, res){
     let tipo_orden = req.query.tipo_orden;
     let pagina = req.query.pagina;
     let cantidad = req.query.cantidad;
+    let offset = pagina * cantidad
 
-    let sql = 'select * from pelicula where genero_id = ? AND anio = ? ORDER BY' + conexion.escapeId(columna_orden, tipo_orden);
-    //titulo LIKE "%?%" AND columna_orden, tipo_orden   ;'
-    
-    conexion.query(sql, [genero, anio], function(error, resultado, fields){
-        if(error){
-            console.log("Hubo un error en la consulta", error.message);
-            return res.status(404).send("Hubo un error en la consulta");
-        }
-        let response = {
-            'peliculas': resultado
-        };
-        res.send(JSON.stringify(response));
-    });
+    if (anio){
+        let sql = 'select * from pelicula where anio = ? ORDER BY ?? LIMIT ? offset ?'
+
+        conexion.query(sql, [anio, columna_orden, tipo_orden, cantidad, offset], function(error, resultado, fields){
+            if(error){
+                console.log("Hubo un error en la consulta", error.message);
+                return res.status(404).send("Hubo un error en la consulta");
+            }
+           // let conteo = resultado.length
+           let conteo = resultado.length
+           console.log(conteo)
+
+            let response = {
+                'peliculas': resultado,
+                'total': conteo,
+            };
+            res.send(JSON.stringify(response));
+    });}
+
+    else if (titulo){
+        let sql = 'select * from pelicula where titulo REGEXP ? ORDER BY ??'
+
+        conexion.query(sql, [titulo, columna_orden, tipo_orden], function(error, resultado, fields){
+            if(error){
+                console.log("Hubo un error en la consulta", error.message);
+                return res.status(404).send("Hubo un error en la consulta");
+            }
+            let response = {
+                'peliculas': resultado
+            };
+            res.send(JSON.stringify(response));
+    });}
+
+    else if (genero){
+        let sql = 'select * from pelicula where genero_id = ? ORDER BY ??'
+
+        conexion.query(sql, [genero, columna_orden, tipo_orden], function(error, resultado, fields){
+            if(error){
+                console.log("Hubo un error en la consulta", error.message);
+                return res.status(404).send("Hubo un error en la consulta");
+            }
+            let response = {
+                'peliculas': resultado
+            };
+            res.send(JSON.stringify(response));
+    });}
+
+    else if (anio && genero){
+        let sql = 'select * from pelicula where genero_id = ? AND anio = ? ORDER BY ??'
+
+        conexion.query(sql, [genero, anio, columna_orden, tipo_orden], function(error, resultado, fields){
+            if(error){
+                console.log("Hubo un error en la consulta", error.message);
+                return res.status(404).send("Hubo un error en la consulta");
+            }
+            let response = {
+                'peliculas': resultado
+            };
+            res.send(JSON.stringify(response));
+        });}
+
+    else if (titulo && genero){
+        let sql = 'select * from pelicula where titulo REGEXP ? AND anio = ? ORDER BY ??'
+
+        conexion.query(sql, [titulo, anio, columna_orden, tipo_orden], function(error, resultado, fields){
+            if(error){
+                console.log("Hubo un error en la consulta", error.message);
+                return res.status(404).send("Hubo un error en la consulta");
+            }
+            let response = {
+                'peliculas': resultado
+            };
+            res.send(JSON.stringify(response));
+        });}
+
+    else if (titulo && anio){
+        let sql = 'select * from pelicula where titulo REGEXP ? AND genero_id = ? ORDER BY ??'
+
+        conexion.query(sql, [titulo, genero, columna_orden, tipo_orden], function(error, resultado, fields){
+            if(error){
+                console.log("Hubo un error en la consulta", error.message);
+                return res.status(404).send("Hubo un error en la consulta");
+            }
+            let response = {
+                'peliculas': resultado
+            };
+            res.send(JSON.stringify(response));
+    });}
+     else {
+        let sql = 'select * from pelicula ORDER BY ??'
+
+         conexion.query(sql, [columna_orden, tipo_orden], function(error, resultado, fields){
+            if(error){
+                console.log("Hubo un error en la consulta", error.message);
+                return res.status(404).send("Hubo un error en la consulta");
+            }
+            let response = {
+                'peliculas': resultado
+            };
+            res.send(JSON.stringify(response));
+    });}
+
 }
-
 
 function traerTodosLosGeneros (req,res) {
     let sql = 'SELECT * FROM genero';
